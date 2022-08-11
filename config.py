bc494e85c3b0ea72ea27e2fb7cf48eba3969b87f
badacash <111057496+badacash@users.noreply.github.com>
2022-08-11 15:13:09 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+config = {
+    'prefix': '>',  # il prefisso per iniziare i comandi
+    'token': """""",  # il token del bot
+    'wagerId': '839774239351373855',  # ID del ruolo wager
+    'guildId': '839773092556570624',  # ID del server discord
+    'adminId': '927617750208221235',  # ID del ruolo STAFF
+    'rulesChannel': '855937611935121440',  # ID  del canale wager-rules
+}
